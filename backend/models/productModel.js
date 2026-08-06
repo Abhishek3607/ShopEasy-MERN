d7eb67c218ea2c3d24724175511de3706c82b5e2
Abhishek <abhishekuhindiholi@gmail.com>
2026-08-06 16:51:13 +0530

    Product controller(get, update, delete product)

---
+++ backend/models/productModel.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "Please enter Product Name"],
+        trim:true  // for removing spaces in name, if any
+    },
+    description:{
+        type:String,
+        required:[true, "Please enter Product Description"],
+    },
+    price:{
+        type:Number,
+        required:[true, "Please enter Product Price"],
+        maxLength:[7, "Price cannot exceed 7 digits"]
+    },
+    ratings:{
+        type:Number,
+        default:0,
+    },
+    image:[
+        {
+            public_id:{
+                type:String,
+                required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    category:{
+        type:String,
+        required:[true, "Please enter Product Category"],
+    },
+    stock:{
+        type:Number,
+        required:[true, "Please enter Product Stock"],
+        maxLength:[5, "Stock cannot exceed 5 digits"],
+        default:1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0,
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            rating:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+export default mongoose.model("Product", productSchema)
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllProducts } from '../controller/productController.js';
+import { createProducts, deleteProduct, getAllProducts, updateProduct } from '../controller/productController.js';
 const router=express.Router();
 
 //Routes
-router.route("/products").get(getAllProducts);
+router.route("/products").get(getAllProducts).post(createProducts);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct)
 
 export default router;
