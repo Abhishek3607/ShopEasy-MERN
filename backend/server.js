1b9669af16991471264ceaf46a8267505363fab2
Abhishek <abhishekuhindiholi@gmail.com>
2026-08-06 23:32:44 +0530

    handling  errors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ import dotenv, { config } from "dotenv";
 import {connectMongoDatabase} from './config/db.js'
 dotenv.config({ path: "backend/config/config.env" });
 connectMongoDatabase();
+
+// Handle uncaught exception errors
+process.on('uncaughtException',(err)=>{
+  console.log(`Error: ${err.message}`);
+  console.log(`Server is shutting down due to uncaught exception errors`);
+  process.exit(1)
+})
+
 const port = process.env.PORT || 3000;
 
 
